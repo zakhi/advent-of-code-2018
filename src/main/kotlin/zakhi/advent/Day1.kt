f1e49397a25a714e44c5c1ea2c87f64dcb04e4ac
Zakhi Verech <zakhi@omnisol.co.il>
2018-12-04 17:52:20 +0200

    Day 1 part 2

--- src/main/kotlin/zakhi/advent/Day1.kt
+++ src/main/kotlin/zakhi/advent/Day1.kt
@@ -8,9 +8,27 @@ package zakhi.advent
 import zakhi.advent.helpers.withResourceStream
 
 fun main() {
-    val sum = withResourceStream("day 1 input.txt") {
-        it.bufferedReader().lineSequence().map(String::toInt).sum()
-    }
+    val frequencyChanges = readFrequencyChanges()
 
-    println(sum)
+    println("Part 1: ${frequencyChanges.sum()}")
+    println("Part 2: ${firstRepeatedFrequency(frequencyChanges)}")
+}
+
+private fun readFrequencyChanges(): List<Int> = withResourceStream("day 1 input.txt") {
+    it.bufferedReader().lineSequence().map(String::toInt).toList()
+}
+
+
+private fun firstRepeatedFrequency(changes: List<Int>): Int {
+    var currentFrequency = 0
+    val uniqueFrequencies = mutableSetOf<Int>()
+
+    while (true) {
+        for (change in changes) {
+            currentFrequency += change
+            if (uniqueFrequencies.contains(currentFrequency)) return currentFrequency
+
+            uniqueFrequencies.add(currentFrequency)
+        }
+    }
 }
