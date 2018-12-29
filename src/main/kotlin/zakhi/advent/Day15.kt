aceaa275eee108521e3494c8f89775ee5e75fe24
Zakhi Verech <zakhiv@gmail.com>
2018-12-29 22:42:05 +0200

    Day 15 (Part 2)

--- src/main/kotlin/zakhi/advent/Day15.kt
+++ src/main/kotlin/zakhi/advent/Day15.kt
@@ -5,28 +5,32 @@ import zakhi.advent.helpers.withResourceStream
 
 
 fun main() {
-    val battlefield: BattleField = withResourceStream("day 15 input.txt") {
-        val input = it.bufferedReader().lineSequence().withIndex().flatMap { (y, line) ->
+    val input = withResourceStream("day 15 input.txt") {
+        it.bufferedReader().lineSequence().withIndex().flatMap { (y, line) ->
             line.withIndex().asSequence().map { (x, char) -> Position(x, y) to char }
         }.toMap()
-
-        createBattlefield(input)
     }
 
-    battlefield.runBattle()
-    println("Part 1: ${battlefield.completedRounds * battlefield.totalHitPointsLeft}")
+    val originalBattleField = createBattleField(input)
+    originalBattleField.runFullBattle()
+    println("Part 1: ${originalBattleField.result}")
+
+    val elvesWonBattleField = generateSequence(4) { it + 1 }
+        .map { createBattleField(input, elfAttackPower = it) }
+        .first { it.runBattleWithoutElfDying() }
+
+    println("Part 2: ${elvesWonBattleField.result}")
 }
 
-
-private fun createBattlefield(input: Map<Position, Char>): BattleField {
+private fun createBattleField(input: Map<Position, Char>, elfAttackPower: Int = 3): BattleField {
     val squaresByPosition = input.filter { it.value != '#' }.mapValues { BattleFieldSquare(it.key) }
 
     squaresByPosition.values.forEach { square ->
-        square.adjacentSquares = square.position.adjacentPositions.mapNotNull { squaresByPosition[it] }.sortedBy { it.position }
+        square.adjacentSquares = square.position.adjacentPositions.mapNotNull { squaresByPosition[it] }
     }
 
     input.filter { it.value in listOf('G', 'E') }.forEach { (position, char) ->
-        val unit = if (char == 'G') Goblin() else Elf()
+        val unit = if (char == 'G') Goblin() else Elf(elfAttackPower)
         unit.moveTo(squaresByPosition[position]!!)
     }
 
@@ -36,64 +40,55 @@ private fun createBattlefield(input: Map<Position, Char>): BattleField {
 
 class BattleField(private val squares: List<BattleFieldSquare>) {
 
-    var completedRounds = 0
-    val totalHitPointsLeft get() = units.sumBy { it.hitPoints }
+    val result get() = completedRounds * units.sumBy { it.hitPoints }
 
-    private val units get() = squares.mapNotNull { it.unit }.sorted()
+    private var completedRounds = 0
 
+    private val units get() = squares.mapNotNull { it.unit }
     private val elfLeft get() = units.any { it is Elf }
     private val goblinLeft get() = units.any { it is Goblin }
+    private val numberOfElves get() = units.count { it is Elf }
 
-    fun runBattle() {
+    fun runFullBattle() {
         while (elfLeft && goblinLeft) {
-            for (unit in units) {
-                if (!unit.dead) {
-                    if (!unit.hasEnemies) return
-
-                    unit.moveCloserToNearestEnemy()
-                    unit.attackNearestEnemy()
-                }
-            }
-
-            completedRounds++
-            printMe()
+            val completed = runNextBattleRound()
+            if (!completed) return
         }
     }
 
-    private val BattleUnit.hasEnemies: Boolean get() = units.any { isEnemy(it) }
+    fun runBattleWithoutElfDying(): Boolean {
+        val elvesStarting = numberOfElves
+        val allElvesSurvived = { elvesStarting == numberOfElves }
 
-    private fun printMe() {
-        println("After round $completedRounds")
-        val squaresByPosition = squares.associateBy { it.position }
-
-        for (y in 0..squares.map { it.position.y }.max()!!) {
-            for (x in 0..squares.map { it.position.x }.max()!!) {
-                val square = squaresByPosition[Position(x, y)]
-
-                if (square == null) {
-                    print('#')
-                } else {
-                    print(
-                        when (square.unit) {
-                            is Elf -> 'E'
-                            is Goblin -> 'G'
-                            null -> '.'
-                        }
-                    )
-                }
-            }
-
-            println()
+        while (goblinLeft || allElvesSurvived()) {
+            val completed = runNextBattleRound()
+            if (!completed) return allElvesSurvived()
         }
 
-        println()
+        return allElvesSurvived()
     }
+
+    private fun runNextBattleRound(): Boolean {
+        for (unit in units) {
+            if (!unit.dead) {
+                if (!unit.hasEnemies) return false
+
+                unit.moveCloserToNearestEnemy()
+                unit.attackNearbyEnemy()
+            }
+        }
+
+        completedRounds++
+        return true
+    }
+
+    private val BattleUnit.hasEnemies: Boolean get() = units.any { isEnemyOf(it) }
 }
-
 
 class BattleFieldSquare(val position: Position) {
 
-    var adjacentSquares: List<BattleFieldSquare> = emptyList()
+    lateinit var adjacentSquares: List<BattleFieldSquare>
+
     var unit: BattleUnit? = null
 
     fun scan(): Sequence<BattleFieldSquare> = sequence {
@@ -114,43 +109,43 @@ class BattleFieldSquare(val position: Position) {
     }
 }
 
-sealed class BattleUnit : Comparable<BattleUnit> {
+sealed class BattleUnit(private val attackPower: Int = 3) : Comparable<BattleUnit> {
 
     var hitPoints = 200
         private set
 
     val dead: Boolean get() = hitPoints <= 0
 
-    abstract fun isEnemy(other: BattleUnit?): Boolean
+    abstract fun isEnemyOf(other: BattleUnit?): Boolean
 
     private var square: BattleFieldSquare? = null
 
     override fun compareTo(other: BattleUnit): Int = compareValuesBy(this, other) { it.square?.position }
 
     fun moveCloserToNearestEnemy() {
-        val closestAttackSquare = checkNotNull(square).scan().find { it.adjacentSquares.any { isEnemy(it.unit) } }
+        val closestAttackSquare = checkNotNull(square).scan().find { it.nearbyEnemies.isNotEmpty() }
         closestAttackSquare.takeIf { it != this.square }?.let { moveTowards(it) }
     }
 
-    fun attackNearestEnemy() {
-        checkNotNull(square).adjacentSquares.mapNotNull { it.unit }.minBy { it.hitPoints }?.hit()
+    fun attackNearbyEnemy() {
+        checkNotNull(square).nearbyEnemies.minBy { it.hitPoints }?.hit(attackPower)
     }
 
     fun moveTo(square: BattleFieldSquare) {
-        if (this.square == square) return
-
         this.square?.unit = null
         this.square = square
         square.unit = this
     }
+
+    private val BattleFieldSquare.nearbyEnemies get() = adjacentSquares.mapNotNull { it.unit }.filter { isEnemyOf(it) }
 
     private fun moveTowards(square: BattleFieldSquare) {
         val thisSquare = checkNotNull(this.square)
         square.scan().find { it in thisSquare.adjacentSquares }?.let { moveTo(it) }
     }
 
-    private fun hit() {
-        hitPoints -= 3
+    private fun hit(power: Int) {
+        hitPoints -= power
 
         if (dead) {
             this.square?.unit = null
@@ -158,10 +153,10 @@ sealed class BattleUnit : Comparable<BattleUnit> {
     }
 }
 
-class Elf : BattleUnit() {
-    override fun isEnemy(other: BattleUnit?): Boolean = other is Goblin
+class Elf(attackPower: Int) : BattleUnit(attackPower) {
+    override fun isEnemyOf(other: BattleUnit?): Boolean = other is Goblin
 }
 
 class Goblin : BattleUnit() {
-    override fun isEnemy(other: BattleUnit?): Boolean = other is Elf
+    override fun isEnemyOf(other: BattleUnit?): Boolean = other is Elf
 }
